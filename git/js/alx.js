5131936c47c5ab02dd9a35807e2b99fa6fb56aed
Sthembiso <skhosanasthe@gmail.com>
2023-07-01 17:37:12 -0700

    Starting to code today, so cool

--- git/js/alx.js
+++ git/js/alx.js
@@ -1,7 +1,1 @@
 console.log("ALX")
-
-root@2ab1c539e10d:/alx_tools/git/js# cat > school.js
-console.log("School")
-root@2ab1c539e10d:/alx_tools# git add .
-root@2ab1c539e10d:/alx_tools/git/js# git add.
-root@2ab1c539e10d:/alx_tools/git/js# git commit -m 'Starting to code today, so cool'
